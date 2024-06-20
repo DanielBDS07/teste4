6a1101d30e505ffaeb97a050e7ac3639097b34a4
daniel <dan.barbosa0702@gmail.com>
2024-06-19 21:15:21 -0300

    paia

--- a.py
+++ a.py
@@ -4,3 +4,15 @@ frutas = ['ameixca', 'abacaxi', 'maça']
 
 for fruta in frutas:
     print(fruta)
+
+
+a = 0 
+while a < 15:
+    print(f'O valoe da variavel é {a}')
+    a +=1
+
+print('codigo finalizou')
+print(f'o valor de a é {a}')
+
+for i in range(2,10):
+    print(i)
